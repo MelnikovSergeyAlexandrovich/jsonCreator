05e3538169f3a33f8017d0e487da793d50159b51
MelnikovSergeyAlexandrovich <isip_s.a.melnikov@gmail.com>
2023-03-29 11:27:02 +0300

    )

---
+++ JsonCreator/Model/colourModel.cs
@@ -0,0 +1,8 @@
+﻿namespace JsonCreator.Model
+{
+    internal class colourModel : IToFindModel
+    {
+        public string name { get; set; }
+        public string hexademical { get; set; }
+    }
+}
